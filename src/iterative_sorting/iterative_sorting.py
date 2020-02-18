1c0a63b27a220fb1fcdb633d5e45db18fddf96e8
Ayomide Jones <ayormeday@gmail.com>
2020-02-18 13:16:00 +0100

    selction sort is resolved

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,23 +6,16 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(1, len(arr) - 1):
-           if arr[j] < arr[cur_index]:
-               cur_index = j
+        for j in range(cur_index + 1, len(arr)):
+           if arr[j] < arr[smallest_index]:
+               smallest_index = j
         
-        if smallest_index != i:
-            arr[cur_index] = arr[i]
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
              
-
+    return arr
 
 
         # TO-DO: swap
-
-
-
-
-    return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
